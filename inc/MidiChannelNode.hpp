56e7b546d40f0972561af062a2aaf7c52ef51234
Lőrinc Serfőző <lorinc.serfozo@mailbox.org>
2022-01-09 12:12:23 +0100

    Channel node II (WIP)

--- inc/MidiChannelNode.hpp
+++ inc/MidiChannelNode.hpp
@@ -6,9 +6,20 @@ namespace mc
 
 class MidiChannelNode final : public Node
 {
+public:
+    MidiChannelNode();
+
 private:
     void render_internal() override;
     channel_map transform_channel_map(const channel_map& in_map) override;
+
+    static const char* get_label(size_t index);
+    static const char* get_hidden_label(size_t index);
+
+    static inline constexpr size_t sm_num_combo_items = 17;
+    static const char* sm_combo_items[sm_num_combo_items];
+
+    std::array<int, 16> m_channels;
 };
 
 }
--- src/MidiChannelNode.cpp
+++ src/MidiChannelNode.cpp
@@ -3,38 +3,91 @@
 #include "imnodes.h"
 
 #include <iomanip>
+#include <numeric>
 #include <sstream>
 
 namespace mc
 {
+
+const char* MidiChannelNode::sm_combo_items[] = {
+    "None", "1", "2", "3", "4", "5", "6", "7", "8",
+    "9", "10", "11", "12", "13", "14", "15", "16"
+};
+
+MidiChannelNode::MidiChannelNode()
+{
+    std::iota(m_channels.begin(), m_channels.end(), 1);
+}
 
 void MidiChannelNode::render_internal()
 {
     imnodes::BeginNodeTitleBar();
     ImGui::TextUnformatted("Channel map");
     imnodes::EndNodeTitleBar();
+    imnodes::BeginInputAttribute(in_id());
+    ImGui::TextUnformatted("MIDI in");
+    imnodes::EndInputAttribute();
+    ImGui::SameLine(135);
+    imnodes::BeginOutputAttribute(out_id());
+    ImGui::TextUnformatted("MIDI out");
+    imnodes::EndOutputAttribute();
     for (size_t i = 0; i < 8; i++)
     {
-        static int val[16];
-        const char* items[] = { "None", "1", "2", "3", "4", "5", "6", "7", "8",
-            "9", "10", "11", "12", "13", "14", "15", "16" };
-        std::stringstream ss;
-        ss << "from ch " << std::setw(2) << i * 2;
-        ImGui::SetNextItemWidth(50);
-        ImGui::Combo(ss.str().c_str(), &val[i * 2], items, 17);
-
-        std::stringstream ss2;
-        ss2 << "from ch " << std::setw(2) << i * 2 + 1;
+        ImGui::TextUnformatted(get_label(i * 2));
         ImGui::SameLine();
         ImGui::SetNextItemWidth(50);
-        ImGui::Combo(ss2.str().c_str(), &val[i * 2 + 1], items, 17);
+        ImGui::Combo(get_hidden_label(i * 2), m_channels.data() + i * 2,
+                     sm_combo_items, sm_num_combo_items);
+
+        ImGui::SameLine();
+        ImGui::TextUnformatted(get_label(i * 2 + 1));
+        ImGui::SameLine();
+        ImGui::SetNextItemWidth(50);
+        ImGui::Combo(get_hidden_label(i * 2 + 1), m_channels.data() + i * 2 + 1,
+                     sm_combo_items, sm_num_combo_items);
     }
-    
+    if (ImGui::Button("Default"))
+    {
+        std::iota(m_channels.begin(), m_channels.end(), 1);
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("Disable all"))
+    {
+        std::fill(m_channels.begin(), m_channels.end(), 0);
+    }
 }
 
 Node::channel_map MidiChannelNode::transform_channel_map(const channel_map& in_map)
 {
     return {};
+}
+
+const char* MidiChannelNode::get_label(size_t index)
+{
+    static std::vector<std::string> labels;
+    if (labels.empty())
+    {
+        for (size_t i = 0; i < sm_num_combo_items; i++)
+        {
+            std::stringstream ss;
+            ss << "ch " << std::setw(2) << i + 1;
+            labels.push_back(ss.str());
+        }
+    }
+    return labels.at(index).c_str();
+}
+
+const char* MidiChannelNode::get_hidden_label(size_t index)
+{
+    static std::vector<std::string> labels;
+    if (labels.empty())
+    {
+        for (size_t i = 0; i < sm_num_combo_items; i++)
+        {
+            labels.push_back("##" + std::string(get_label(i)));
+        }
+    }
+    return labels.at(index).c_str();
 }
 
 }

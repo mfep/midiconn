4bc55dbe15333478889fc42fa7480ba697e2fd9d
Lőrinc Serfőző <lorinc.serfozo@mailbox.org>
2022-02-25 20:56:56 +0100

    Animating MIDI activity indicator

--- src/MidiInNode.cpp
+++ src/MidiInNode.cpp
@@ -4,7 +4,6 @@
 #include "imgui.h"
 #include "imnodes.h"
 #include "MidiEngine.hpp"
-#include "spdlog/spdlog.h"
 
 namespace mc
 {
@@ -25,27 +24,31 @@ MidiInNode::~MidiInNode()
 
 void MidiInNode::render_internal()
 {
-    using namespace std::literals;
     imnodes::BeginNodeTitleBar();
     ImGui::TextUnformatted(m_input_info.m_name.c_str());
     imnodes::EndNodeTitleBar();
     imnodes::BeginOutputAttribute(out_id());
-    const bool active = std::chrono::system_clock::now() - m_last_message_received < 1s;
-    if (active)
-    {
-        const auto color = ImVec4(0, 255, 0, 255);
-        ImGui::PushStyleColor(ImGuiCol_Button, color);
-        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, color);
-        ImGui::PushStyleColor(ImGuiCol_ButtonActive, color);
-    }
+
+    constexpr float r_component = 0;
+    constexpr float g_component = 1;
+    constexpr float b_component = 0;
+    constexpr double fade_time_ms = 1000;
+
+    const auto ms_since_last_message = std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now() - m_last_message_received).count();
+    const double percent = 1 - std::min(1., ms_since_last_message / fade_time_ms);
+    const auto color = ImVec4(0, 1, 0, percent);
+    ImGui::PushStyleColor(ImGuiCol_Button, color);
+    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, color);
+    ImGui::PushStyleColor(ImGuiCol_ButtonActive, color);
+    ImGui::PushStyleVar(ImGuiStyleVar_DisabledAlpha, 1);
     ImGui::BeginDisabled();
     ImGui::Button("  ");
     ImGui::EndDisabled();
-    if (active)
-    {
-        ImGui::PopStyleColor(3);
-    }
+    ImGui::PopStyleVar();
+    ImGui::PopStyleColor(3);
     ImGui::SameLine();
+
     ImGui::TextUnformatted("all channels");
     imnodes::EndOutputAttribute();
 }

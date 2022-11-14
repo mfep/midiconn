e94e826f06850a2180c5753e4bee721c17df7053
Lőrinc Serfőző <lorinc.serfozo@mailbox.org>
2022-11-14 21:33:18 +0100

    Renamed app to midiconn

--- inc/ApplicationName.hpp
+++ inc/ApplicationName.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
-#define MIDI_APPLICATION_NAME       "MIDI Connector"
-#define MIDI_APPLICATION_NAME_SNAKE "midi_connector"
-#define MIDI_APPLICATION_ORG        "mfep"
+#define MIDI_APPLICATION_NAME       "midiconn"
+#define MIDI_APPLICATION_NAME_SNAKE "midiconn"
+#define MIDI_APPLICATION_ORG        "xyz.safeworlds"
